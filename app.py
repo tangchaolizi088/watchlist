df699c633d1bf3ec3785bf5da79046d0b82914e7
wangdx <348759948@qq.com>
2021-05-09 22:03:48 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 from flask import Flask
-from flask import escape,url_for
+from flask import escape,url_for,render_template
 app = Flask(__name__)
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 @app.route('/user/<name>')
 @app.route('/')
 @app.route('/home')
-def hello():
-    return 'Welcome to My Watchlist!'
-
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User:%s' % escape(name)
+def index():
+    return render_template('index.html',name = name,movies = movies)
 
 
 
-@app.route('/test')
-def test_url_for():
-    print(url_for('hello'))
-    print(url_for('user_page',name = 'greyLi'))
-    return 'Test Page'
 
 
 
