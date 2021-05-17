f5743a521cfbd145b4eb15982fadaf356b58d8a8
wangdx <348759948@qq.com>
2021-05-17 22:57:20 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask import escape, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
 import click
-import os,sys
+import os, sys
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -16,20 +16,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
 
-
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
 
+
 class Movie(db.Model):
-    id = db.Column(db.Integer,primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
-
-
 
 
 @app.cli.command()  # 注册为命令
@@ -39,6 +37,7 @@ def initdb(drop):
         db.drop_all()
     db.create_all()
     click.echo('Initialized database.')
+
 
 @app.cli.command()
 def forge():
@@ -56,10 +55,10 @@ def forge():
         {'title': 'WALL-E', 'year': '2008'},
         {'title': 'The Pork of Music', 'year': '2012'},
     ]
-    user = User(name = name)
+    user = User(name=name)
     db.session.add(user)
     for m in movies:
-        movie = Movie(title = m['title'],year = m['year'])
+        movie = Movie(title=m['title'], year=m['year'])
         db.session.add(movie)
     db.session.commit()
     click.echo('Done.')
@@ -67,9 +66,19 @@ def forge():
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
 
 
 if __name__ == '__main__':
